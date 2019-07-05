5faabbc82f5ed8dde6f69d6b27ddebcd00d32481
Simon Garber <Simon.garber@gmail.com>
2019-07-05 17:03:21 -0400

    implemented the ISS Flyover times function and successfully logged the results with the error conditions

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss')
-// ***************************************************************//
+const { fetchISSFlyOverTimes } = require('./iss')
+// ***********************
+// Fetch IP function Call*
+// ***********************
 fetchMyIP((error, ip)=> {
     if(error) {
         console.log("It didn't work!" , error);
         return;
     } else {
         console.log("It works!, Returned IP: ", ip);
-//****************************************************************//  
+
         fetchCoordsByIP(ip,(error,data) => {
             if(error) {
                 console.log("It didn't work!" , error);
                 return;
             } else {
-                console.log("It works!, Returned Coordinates: ", data); 
-            }   
-        });
-    }
-});
-    
-        
-                
-
+                console.log("It works!, Returned Coordinates: ", data);
+                const coords = {latitude: data.latitude, longitude: data.longitude} 
+                fetchISSFlyOverTimes(coords,(error,passTimes) => {
+                    if(error) {
+                        console.log("It didn't work!" , error);
+                            return;
+                        } else {
+                            console.log("It works!, Returned Flyer Over Times: ", passTimes);
+                    }
+                })  
+                }
+            }) 
+        }
+    })   
--- iss.js
+++ iss.js
@@ -1,5 +1,8 @@
 
 const request = require('request');
+// ******************************
+// Fetch IP function declaration*
+// ******************************
 const fetchMyIP = function(callback) {
    const url = (`https://api.ipify.org?format=json`) 
    request(url,function(err,resp,body) {
@@ -17,6 +20,9 @@ const fetchMyIP = function(callback) {
       callback(null,ip); 
     })
   };
+// ***************************************
+// Fetch coordinates function declaration*
+// ***************************************
   const fetchCoordsByIP = function(ip,callback) {
     const url = `https://ipvigilante.com/${ip.ip}`
     request(url,function(err,resp,body){
@@ -36,7 +42,28 @@ const fetchMyIP = function(callback) {
       callback(null,coordinates)
     })
   }
-  module.exports = { fetchMyIP,fetchCoordsByIP };
+  // ************************
+  // Fetch ISS FlyOver Times*
+  // ************************
+
+  const fetchISSFlyOverTimes = function(coords, callback) {
+    const url = `http://api.open-notify.org/iss-now.json?lat=${coords.latitude}lon=${coords.longitude}`;
+    request(url,function(err,resp,body) {
+    if(err) {
+      callback(err,null)
+      return
+    }
+    if(resp.statusCode !== 200) {
+      console.log(`HTTP Status code: ${resp.statusCode}`)   
+      callback(error(`Status Code ${resp.statusCode} when fetching ISS passes. Response: ${body}`), null);
+      return;
+  }
+    const passes = JSON.parse(body)
+    
+    callback(null,passes)
+  });
+};
+  module.exports = { fetchMyIP,fetchCoordsByIP,fetchISSFlyOverTimes };
         
   
        
