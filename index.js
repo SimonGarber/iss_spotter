059ddc5bd1ee76f9e819eb109065def19cbf7219
Simon Garber <Simon.garber@gmail.com>
2019-07-05 13:29:31 -0400

    both functions are working and fetching and logging the correct data from each API. Each function has correct error handling logic.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const { fetchMyIP } = require('./iss');
-
+const { fetchCoordsByIP } = require('./iss')
+// ***************************************************************//
 fetchMyIP((error, ip)=> {
     if(error) {
         console.log("It didn't work!" , error);
         return;
     } else {
-        console.log("It works!, Returned IP: ", ip); 
+        console.log("It works!, Returned IP: ", ip);
+//****************************************************************//  
+        fetchCoordsByIP(ip,(error,data) => {
+            if(error) {
+                console.log("It didn't work!" , error);
+                return;
+            } else {
+                console.log("It works!, Returned Coordinates: ", data); 
+            }   
+        });
     }
-    
 });
+    
+        
+                
+
--- iss.js
+++ iss.js
@@ -1,35 +1,48 @@
-/** 
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- 
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
 
- const fetchMyIP = function(callback) {
-  find the URL for retrieving our IPv4 IP address in JSON format
-  */
 const request = require('request');
-
- const fetchMyIP = function(callback) {
-    const url = (`https://api.ipify.org?format=json`) 
-    request(url,function(err,resp,body) {
-    const ip = JSON.parse(body)
-    if(!err) {
-      callback(null,ip); 
-      
-      console.log(`HTTP Status code: ${resp.statusCode}`)   
-    } else {
-      callback(err,null);
-    if(resp.statusCode !== 200) {
+const fetchMyIP = function(callback) {
+   const url = (`https://api.ipify.org?format=json`) 
+   request(url,function(err,resp,body) {
+     if(err) {
+       console.log("there was a problem: ", err)
+       callback(err,null)
+       return;
+      } 
+      if(resp.statusCode !== 200) {
+        console.log(`HTTP Status code: ${resp.statusCode}`)   
         callback(error(`Status Code ${resp.statusCode} when fetching IP. Response: ${body}`), null);
-        return
-    }
-    console.log("there was a problem: ", err)
-    }
+        return;
+      }
+      const ip = JSON.parse(body)
+      callback(null,ip); 
+    })
+  };
+  const fetchCoordsByIP = function(ip,callback) {
+    const url = `https://ipvigilante.com/${ip.ip}`
+    request(url,function(err,resp,body){
+  
+      if(err) {
+        console.log("there was a problem: ", err)
+        callback(err,null)
+        return;
+      } 
+      if(resp.statusCode !== 200) {
+        console.log(`HTTP Status code: ${resp.statusCode}`)   
+        callback(error(`Status Code ${resp.statusCode} when fetching coordinates. Response: ${body}`), null);
+        return;
+      }
+      const info = JSON.parse(body)
+      const coordinates = {latitude: info.data.latitude, longitude: info.data.longitude}
+      callback(null,coordinates)
+    })
+  }
+  module.exports = { fetchMyIP,fetchCoordsByIP };
         
-    }); 
-}
+  
+       
+    
+   
 
-module.exports = { fetchMyIP };
+
+
+
